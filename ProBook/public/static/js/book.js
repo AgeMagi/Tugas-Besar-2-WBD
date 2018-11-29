53dce42edf5967a5c572b5707ed09a315166872c
thareqmy <thareqmyha@gmail.com>
2018-11-29 14:52:11 +0700

    fix bug from browse.html

--- ProBook/public/static/js/book.js
+++ ProBook/public/static/js/book.js
@@ -1,14 +1,27 @@
 angular.module('myApp', []).controller('bookCtrl', function($scope) {
 
-    $scope.books = [
+    $scope.bookbases = [
         {id:1, img_path:'/static/img/contoh_buku.png', title:'Merangkai Mimpi', author:'thareq', rating:'4', description:'Satu kata yaitu mantuy bet gakuat kaka'},
         {id:2, img_path:'/static/img/contoh_buku.png', title:'Untuk Masa Depan Kita', author:'habibi', rating:'5', description:'Kenangan begitu indah untukmu sayang'},
         {id:3, img_path:'/static/img/contoh_buku.png', title:'Demi Kehidupan yang Indah', author:'yasya', rating:'5', description:'Pege'}
     ];
 
+    $scope.books = [];
+
     $scope.search = false;
     $scope.searchQuery = function(query) {
         $scope.search = true;
+        $scope.books = [];
+
+
+        //$scope.books.push(query);
+        for(var i = 0, size = $scope.bookbases.length; i < size ; i++){
+            var item = $scope.bookbases[i];
+            //$scope.books.push(item);
+            if ((item.title).indexOf(query) >=0) {
+                $scope.books.push(item);
+            }
+        }
 
     };
 
