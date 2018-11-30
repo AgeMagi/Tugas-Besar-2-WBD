9fc35fa7154abc828d97f7dca4d3cdd52ba9658e
Yasya <yasya.rusyda@gmail.com>
2018-11-30 10:05:01 +0700

    buybook in process

--- WebServiceBook/src/service/BookServiceImpl.java
+++ WebServiceBook/src/service/BookServiceImpl.java
@@ -421,7 +421,7 @@ public class BookServiceImpl implements  BookService {
 
     public TransferStatus checkTransfer(String senderCard, Integer price ){
         String urlParameters ="sender_card_number"+ senderCard + "&amount=" + price;
-        String url = "http://localhost:8000/transacton";
+        String url = "http://localhost:8000/transaction";
         HttpURLConnection connection;
         TransferStatus transferStatus = null;
         connection = null;
